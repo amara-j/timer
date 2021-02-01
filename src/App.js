4ba0a4c85048e05ac413ef346ab03a1fc49f72c5
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-01 12:53:32 -0500

    add number input, doesn't yet adjust number of seconds for timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Stopwatch from "./Countdown.js";
+import { useState } from "react";
 
 const App = () => {
+  const [countTo, setCountTo] = useState(0);
+
   return (
     <div className="App">
-      Timer
+      Timer!
+      <div>
+        <label>
+          Time (seconds)
+          <input
+            type="number"
+            value={countTo}
+            onChange={(e) => setCountTo(e.target.value)}
+          ></input>
+        </label>
+      </div>
       <Stopwatch />
     </div>
   );
--- src/Countdown.js
+++ src/Countdown.js
@@ -2,43 +2,39 @@ import { useState } from "react";
 
 const Stopwatch = () => {
   const [timeElapsed, updateTimeElapsed] = useState(0);
+  const [isStopped, setIsStopped] = useState(false);
 
   const calculateTime = () => {
     let startTime = Date.now();
-
-    const timeCounter = (timer, max, repeatArgument, callbackArgument) => {
-      var counter = 1;
-      var init = (t) => {
-        let timeStart = Date.now();
-        setTimeout(function () {
-          if (counter < max) {
-            let fix = Date.now() - timeStart - timer;
-            init(t - fix);
-            counter++;
-            // repeat this event max times
-            repeatArgument();
-          } else {
-            // execute this event when timer ends
-            callbackArgument();
-          }
-        }, t);
-      };
-      init(timer);
+    var counter = 1;
+    const timeCounter = (max) => {
+      let timeStart = Date.now();
+      setTimeout(() => {
+        if (counter < max) {
+          let fix = Date.now() - timeStart - 1000;
+          timeCounter(1000 - fix);
+          counter++;
+          updateTimeElapsed(Math.floor((Date.now() - startTime) / 1000));
+        } else {
+          // execute this event when timer ends
+          updateTimeElapsed("Timer done!");
+        }
+      }, 1000);
     };
-    return timeCounter(
-      1000,
-      20,
-      () => {
-        updateTimeElapsed(Math.floor((Date.now() - startTime) / 1000));
-      },
-      () => {
-        updateTimeElapsed("Timer done!");
-      }
-    );
+
+    return <div>{timeCounter(20)}</div>;
   };
+
   return (
     <div>
-      <button onClick={() => calculateTime()}>Start</button>
+      <button
+        onClick={() => {
+          calculateTime();
+        }}
+      >
+        Start
+      </button>
+      <button onClick={() => setIsStopped(true)}>Stop</button>
       <div>{timeElapsed}</div>
     </div>
   );
