8565cdafb8f887c71cdfdd9759df1df2743e9796
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-01 12:02:15 -0500

    Initial commit- hopefully accurate timer logs to console on start button click

---
+++ src/Countdown.js
@@ -0,0 +1,43 @@
+// import { useState } from "react";
+
+const Stopwatch = () => {
+  //   const [timeElapsed, updateTimeElapsed] = useState(0);
+
+  const calculateTime = () => {
+    let startTime = Date.now();
+
+    const timeCounter = (timer, max, repeatArgument, callbackArgument) => {
+      var counter = 1;
+      var init = (t) => {
+        let timeStart = Date.now();
+        setTimeout(function () {
+          if (counter < max) {
+            let fix = Date.now() - timeStart - timer;
+            init(t - fix);
+            counter++;
+            // repeat this event max times
+            repeatArgument();
+          } else {
+            // execute this event when timer ends
+            callbackArgument();
+          }
+        }, t);
+      };
+      init(timer);
+    };
+    return timeCounter(
+      1000,
+      20,
+      () => {
+        console.log(Math.floor((Date.now() - startTime) / 1000));
+      },
+      () => {
+        console.log("Timer done!");
+      }
+    );
+  };
+
+  return <button onClick={() => calculateTime()}>Start</button>;
+};
+
+export default Stopwatch;
