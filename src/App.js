1585388dcebef3aee03cb505f858e401cda9b32e
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-03 11:51:49 -0500

    number input shows countdown on submit and logs the inputted number

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 import Timer from "./NewTimer.js";
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 
 const App = () => {
   const dateOnLoad = new Date();
+  const timerValue = useRef(null);
+
   const [timerIsSet, updateTimerIsSet] = useState(false);
+  // const [timerValue, setTimerValue] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(timerValue.current.value);
+    updateTimerIsSet(true);
+  };
 
   return (
     <div className="App">
-      <button onClick={() => updateTimerIsSet(true)}>Show Timer</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="number" name="name" ref={timerValue} />
+        </label>
+        <input type="submit" value="Start" />
+      </form>
       {timerIsSet ? <Timer dateOnLoad={dateOnLoad} /> : null}
     </div>
   );
